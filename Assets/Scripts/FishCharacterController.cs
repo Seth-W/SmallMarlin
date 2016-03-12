2221c39741fb7ea3804f4bb49d18fb33bec0f318
Seth <s.weinberg62@gmail.com>
2016-03-11 20:38:30 -0500

    Character controller w/ screen bounds

---
+++ Assets/Scripts/FishCharacterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishCharacterController : MonoBehaviour
+{
+    Rigidbody rb;
+    [Range(1,20)]
+    public float scalar;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(scalar * checkInput());
+    }
+
+    Vector3 checkInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        return new Vector3(x, y, 0);
+    }
+
+}
--- Assets/Scripts/Fish_Player.cs
+++ Assets/Scripts/Fish_Player.cs
@@ -2,20 +2,50 @@
 using System;
 
 public class Fish_Player : Fish {
-
+    public int playerInitSize;
     public static event EventHandler<InfoEventArgs<int>> playerDeathEvent;
-
+    Rigidbody rb;
 	// Use this for initialization
 	void Start ()
     {
-        setSize(100);
+        setSize(playerInitSize);
         init();
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate ()
+    {
+        Vector3 pos = gameObject.transform.position;
+        Vector3 vel = rb.velocity;
+        if (pos.x > 10)
+        {
+            pos.x = -10;
+            gameObject.transform.position = pos;
+        }
+        else if (pos.x < -10)
+        {
+            pos.x = 10;
+            gameObject.transform.position = pos;
+        }
+        
+        if (pos.y > 4.85)
+        {
+            pos.y = 4.850f;
+            gameObject.transform.position = pos;
+            vel.y = 0;
+            rb.velocity = vel;
+            
+        }
+        else if (pos.y < -4.85)
+        {
+            pos.y = -4.850f;
+            gameObject.transform.position = pos;
+            vel.y = 0;
+            rb.velocity = vel;
+        }
+
+    }
     protected override void eat(Fish other)
     {
         if (_size <= other.size)
